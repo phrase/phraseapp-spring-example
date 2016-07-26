3176ad3d49cffb8cddbb5e52a510c61061a7a77c
docstun <manuel.boy@dynport.de>
2016-07-26 12:42:21 +0200

    Replace demo project id to avoid confusion

--- src/main/java/com/phraseapp/incontexteditor/PhraseAppConfiguration.java
+++ src/main/java/com/phraseapp/incontexteditor/PhraseAppConfiguration.java
@@ -2,7 +2,7 @@ package com.phraseapp.incontexteditor;
 
 public class PhraseAppConfiguration {
     public static final boolean PHRASEAPP_ENABLED = false;
-    public static final String PHRASEAPP_PROJECT_ID = "00000000000000004158e0858d2fa45c";
+    public static final String PHRASEAPP_PROJECT_ID = "00000000000000000000000000000000";
     public static final String PHRASEAPP_PREFIX = "{{__";
     public static final String PHRASEAPP_SUFFIX = "__}}";
 }
